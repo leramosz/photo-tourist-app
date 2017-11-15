73ac157fcd8064331daf80e046f3ead8a906c2bf
Leonardo Ramos <Leo@Leonardos-MBP.iptv.microsoft.com>
2017-11-15 18:28:40 -0300

    api explanation page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "pages#index"
   scope :api, defaults: {format: :json} do
     resources :cities, except: [:new, :edit]
     resources :states, except: [:new, :edit]
